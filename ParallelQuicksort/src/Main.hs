6733fc42259251ffe9ac6880a031dc367a081bae
danimal4580 <danielknight2019@u.northwestern.edu>
2019-01-12 16:32:02 -0600

    first draft

--- ParallelQuicksort/src/Main.hs
+++ ParallelQuicksort/src/Main.hs
@@ -3,3 +3,19 @@ module Main where
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
+
+Quicksort :: [Int] -> [Int]
+Quicksort x:[] = [x]
+Quicksort (x:xs) = 
+    let pivot = Pickapivot(x:xs) in 
+        lessthan++equals++greaterthan
+        where
+            lessthan = Quicksort [ x | x <- (x:xs), x < pivot]
+            greaterthan = Quicksort [ x | x <- (x:xs), x > pivot]
+            equals = [ x | x <- (x:xs), x == pivot]
+
+Pickapivot :: [Int] -> Int
+Pickapivot (x:xs) = last xs
+
+
+
